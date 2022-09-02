ac4999127a207bb9c55c09a2bbe3c47c0364f0f9
skys215 <skys215@gmail.com>
2022-09-02 14:44:23 +0800

    Fixed bug, lang inside {{

--- views/templates/scaffold/show_field.blade.php
+++ views/templates/scaffold/show_field.blade.php
@@ -1,11 +1,11 @@
 <!-- {{ $fieldTitle }} Field -->
 <div class="form-group mb-3 row">
   @if($config->options->localized)
-    <h6 class="col s3">@{{__('models/{{ $config->modelNames->camelPlural }}.fields.{{ $fieldName }}')}}:</h6>
+    <h6 class="col s3">{{ '{{' }}__('models/{{ $config->modelNames->camelPlural }}.fields.{{ $fieldName }}')}}:</h6>
   @else
     <h6 class="col s3">{{ $fieldTitle }}:</h6>
   @endif
   <h6 class="col s9">
-    @{{ ${!! $config->modelNames->camel !!}->{!! $fieldName !!} }}
+    {{ '{{' }} ${!! $config->modelNames->camel !!}->{!! $fieldName !!} }}
   </h6>
 </div>
